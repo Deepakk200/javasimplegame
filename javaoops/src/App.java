8a8d409b8222eb4e12cb8fd9856782d55110e1cf
Deepakk200 <126246500+Deepakk200@users.noreply.github.com>
2024-03-02 11:56:50 +0530

    Add files via upload
    SNAKE GAME - implemented in java .
    completed half of the logic .

---
+++ javaoops/src/App.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+public class App{
+    public static void main(String[] args) {
+        int boardwidth = 600;
+        int boardHeight = boardwidth;
+    
+    JFrame frame = new JFrame("snake"); 
+    frame.setVisible(true);
+    frame.setSize(boardHeight, boardwidth);
+    frame.setLocationRelativeTo(null);
+    frame.setResizable(false);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    SnakeGame snakeGame = new SnakeGame(boardwidth, boardHeight);
+    frame.add(snakeGame);
+    frame.pack();
+}
+
+}
